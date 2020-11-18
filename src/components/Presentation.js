c9116e85c7fc8c5b1fcc1349f475ef66b278089f
gutovesco <guto.vesco@hotmail.com>
2020-11-18 12:13:00 -0300

    newsletter email

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import axios from 'axios'
+
 const Presentation = () => {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSubmit = async () => {
+      const NEWSLETTER_REST_API_URL = 'http://localhost:8080/api/newsletter';
+
+      const data = {
+        email: inputValue
+      }
+
+      console.log(data)
+    
+    await axios.post(NEWSLETTER_REST_API_URL, data).then(res => {
+      console.log(res)
+    })
+  }
+
     return(
         <div>
             <section id="home" className="min-h-100vh flex justify-start items-center">
@@ -8,10 +27,10 @@ const Presentation = () => {
                 <h1 className="white fs-l3 lh-2 md-fs-xl1 md-lh-1 fw-900 ">Acompanhe a COVID-19 <br />próxima a você!</h1>
 
                 <div className="br-8 mt-10 inline-flex">
-                  <input type="text"
+                  <input type="text" onChange={(e) => setInputValue(e.target.value)} value={inputValue}
                     className="input-lg half bw-0 fw-200 bg-indigo-lightest-20 indigo-lightest ph-indigo-lightest focus-white opacity-80 fs-s3 py-5 min-w-25vw br-r-0"
                     placeholder="Endereço de email" />
-                  <button
+                  <button onClick={() => handleSubmit()}
                     className="button-lg bg-white black focus-white fw-300 fs-s3 mr-0 br-l-0">Enviar</button>
                 </div>
               </div>
